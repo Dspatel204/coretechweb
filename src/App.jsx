31e62494c4e0e5c13bc1251c3fe50865bb66fc51
Dspatel204 <dishant.sureshbhai@gmail.com>
2024-09-22 14:50:52 +0530

    addd a data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import React from 'react'
 import Home from "./pages/Home/Home.jsx";
 import About from "./pages/About/About.jsx";
 import Portfolio from "./pages/Portfolio/Portfolio.jsx";
